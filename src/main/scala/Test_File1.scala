0a9a8a8ba4619a223229c2d6bf9100e74869b4ae
rautritesh785-sudo <rautritesh785@gmail.com>
2026-01-06 07:43:41 +0530

    Thrid Commit

--- src/main/scala/Test_File1.scala
+++ src/main/scala/Test_File1.scala
@@ -1,46 +1,47 @@
-object Test_File {
-//  println("Second Scala Program")
-//  println("Third Scala Program")
-//
-////  val a=10
-////  println(a)
-////  var b=20
-////  println(b)
-////  b=30
-////  b=60
-////  println(b)
-//
-//  var c=a+b
-//  println(c)
-//
-//  val age:Int=25
-//  val name:String="Ritesh"
-//  val grade:Char='A'
-//  val distance:Long= 7721980390L
-//  println(age,name,grade,distance)
-//
-//  val myvalue: Any = 30
-//  println(myvalue)
-//var A:Any=10
-//  A=20
-//  println(A)
-//
-//  val isScalaFun:Boolean=false
-//  val isJavaDiff:Boolean=false
-//  if(isJavaDiff)
-//   {
-//      println("Scala is Fun!")
-//    }
-//val a=20
-//  val b=30
-//  println(a==b)
-//  println(a>b)
-//  println(a<b)
-//  println(a<=b)
-//  println(a>=b)
-//  println(a!=b)
-//
-//
+object Test_File1 {
+  println("Second Scala Program")
+  println("Third Scala Program")
+
+  val a=10
+  println(a)
+  var b=20
+  println(b)
+  b=30
+  b=60
+  println(b)
+
+  var c=a+b
+  println(c)
+
+  val age:Int=25
+  val name:String="Ritesh"
+  val grade:Char='A'
+  val distance:Long= 7721980390L
+  println(age,name,grade,distance)
+  println(age,name,grade)
+
+  val myvalue: Any = 30
+  println(myvalue)
+var A:Any=10
+  A=20
+  println(A)
+
+  val isScalaFun:Boolean=false
+  val isJavaDiff:Boolean=false
+  if(isJavaDiff)
+   {
+      println("Scala is Fun!")
+    }
+  val e=20
+  val d=30
+  println(e==d)
+  println(e>d)
+  println(e<d)
+  println(e<=d)
+  println(e>=d)
+  println(e!=d)
+
+
 val x=10
   val y=5
   if (x>0 && y>0)
